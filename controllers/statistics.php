c8079f6bb0492df5f5b3b1fccef617a42d6e92a0
ubius67 <shishlov2008@yandex.ru>
2018-12-19 23:12:15 +0300

    MOD: добавил класс-парсер csv

--- controllers/statistics.php
+++ controllers/statistics.php
@@ -1,105 +1,62 @@
 <?php
 require_once 'controllers/common.php';
 require_once 'models/statistics.php';
+require_once 'models/csv.php';
 
 class Statistics extends Common {
 	
 	private $_model = null;
 	
-	const FIELDS = [
-		0 => 'sezon',
-		1 => 'is_home',
-		2 => 'min',
-		3 => 'z2',
-		4 => 'b2',
-		5 => 'z3',
-		6 => 'b3',
-		7 => 'z23',
-		8 => 'b23',
-		9 => 'qw',
-		10 => 'we',
-		11 => 'er',
-		12 => 'rt',
-		13 => 'ty',
-		14 => 'yu',
-		15 => 'ui',
-		16 => 'io',
-		17 => 'as',
-		18 => 'sd',
-		19 => 'df',
-		20 => 'fg',
-		21 => 'gh',
-		22 => 'hj',
-		23 => 'jk',
-		24 => 'kl',
-		25 => 'la',
-	];
-	
 	public function __construct() {
 		$this->_model = new StatisticsModel();
 	}
 
+	/**
+	 * Страница с формой сохранения протокола статистики
+	 */
 	public function index() {
 		$this->display('index.tpl');
 	}
-	
+
+	/**
+	 * Сохранить протокол статистики
+	 */
 	public function save() {
-		setlocale(LC_ALL, 'en_US.utf8');
-		$csv = [];
-
-		// check there are no errors
-		if($_FILES['csv']['error'] == 0){
-			$name = $_FILES['csv']['name'];
-			$ext = strtolower(end(explode('.', $_FILES['csv']['name'])));
-			$type = $_FILES['csv']['type'];
-			$tmpName = $_FILES['csv']['tmp_name'];
-
-			// check the file is a csv
-			if($ext === 'csv'){
-				if(($handle = fopen($tmpName, 'r')) !== false) {
-					// necessary if a large csv file
-					set_time_limit(0);
-
-					$row = 0;
-
-					while(($data = fgetcsv($handle, 1000)) !== false) {
-						$data = array_map("utf8_encode", $data);
-						// number of fields in the csv
-						$col_count = count($data);
-
-						// get the values from the csv
-						$csv[] = $data[0];
-
-						// inc the row
-						$row++;
-					}
-					fclose($handle);
-				}
-			}
-		}
-
-		
-		$dataArray = [];
-		foreach ($csv as $dataString) {
-			$dataArray[] = explode(';', $dataString);
-		}
-		
-		$finalData = [];
-		
-		foreach ($dataArray as $key1 => $row) {
-			foreach ($row as $key2 => $value) {
-				
-				$finalData[$key1][self::FIELDS[$key2]] = $value;
-			}
-		}
-		$this->vd($finalData);exit;
+		$Csv = new Csv($_FILES['csv']);
+		$data = $Csv->getArray();
+		$data = $this->_getCorrectKeyArray($data);
+		$this->vd($data);exit;
 	}
 
+	/**
+	 * Получить статистику
+	 */
 	public function get() {
 		$data = $this->_model->get();
 		echo '<pre>';
 		var_dump($data);
 		echo '</pre>';
 		exit;
+	}
+
+	/**
+	 * Установка корректных названий элементам массива
+	 * 
+	 * @param array $data
+	 * @return array
+	 */
+	private function _getCorrectKeyArray($data) {
+		if (!$data) {
+			return [];
+		}
+
+		$finalData = [];
+		foreach ($data as $key1 => $row) {
+			foreach ($row as $key2 => $value) {
+				$finalData[$key1][StatisticsModel::FIELDS[$key2]] = mb_convert_encoding($value, "utf-8", "windows-1251");
+			}
+		}
+
+		return $finalData;
 	}
 }
--- models/statistics.php
+++ models/statistics.php
@@ -2,6 +2,35 @@
 require_once 'models/main.php';
 
 class StatisticsModel extends Main {
+
+	const FIELDS = [
+		0 => 'sezon',
+		1 => 'is_home',
+		2 => 'min',
+		3 => 'z2',
+		4 => 'b2',
+		5 => 'z3',
+		6 => 'b3',
+		7 => 'z23',
+		8 => 'b23',
+		9 => 'qw',
+		10 => 'we',
+		11 => 'er',
+		12 => 'rt',
+		13 => 'ty',
+		14 => 'yu',
+		15 => 'ui',
+		16 => 'io',
+		17 => 'as',
+		18 => 'sd',
+		19 => 'df',
+		20 => 'fg',
+		21 => 'gh',
+		22 => 'hj',
+		23 => 'jk',
+		24 => 'kl',
+		25 => 'la',
+	];
 	
 	public function save() {
 		var_dump('Сохранение');exit;
