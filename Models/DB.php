0bc4b8095957450760b055f6a959efacbfd6c5a4
Ivan Shishlov <ivansh@playtika.com>
2020-05-28 16:36:24 +0300

    MOD: правка по подключению к бд

--- Models/DB.php
+++ Models/DB.php
@@ -19,11 +19,12 @@ class DB {
 
 	private function connect() {
         $config =  parse_ini_file('../config.ini', true);
+        $port = $config['SQL']['port'] ? ';port=' . $config['SQL']['port'] : '';
 
 	    $debug = self::DEBUG_ENABLED ? [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] : [];
 		try {
 			$this->_db = new PDO(
-				'mysql:dbname=' . $config['SQL']['name'] . ';host=' . $config['SQL']['host'] . ';port=' . $config['SQL']['port'],
+				'mysql:dbname=' . $config['SQL']['name'] . ';host=' . $config['SQL']['host'] . $port,
                 $config['SQL']['login'],
                 $config['SQL']['password'],
                 $debug
