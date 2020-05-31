9d0089c003d63dff14889dbb5967df796b725c13
Ivan Shishlov <ivansh@playtika.com>
2020-05-31 23:52:32 +0300

    MOD: фикс бага в роуте

--- Route.php
+++ Route.php
@@ -15,7 +15,7 @@ class Route {
             $method = $methodArray[0];
 		}
 
-		$file = mb_strtolower('controllers/' . $controllerName . '.php');
+		$file = 'controllers/' . $controllerName . '.php';
 		if (!file_exists($file)) {
 			header("HTTP/1.0 404 Not Found");
 			//print('Данной страницы не существует!');
